25a54ae01ba411e6c71a6e9640243e006ebc84a8
Alain Chevanier <alain.chevanier@nubank.com.br>
2024-01-23 20:35:20 -0600

    Rearrenging function examples

--- src/clojure_noob/functions.clj
+++ src/clojure_noob/functions.clj
@@ -1,40 +1,52 @@
 (ns clojure-noob.functions
   (:require clojure.set))
 
-;; multi-arity functions
+;; ways to define a function
+(def square-1
+  (fn [x]
+    (println "calculating the square of x: " x)
+    (* x x)))
 
-(def make-set-1
-  (fn [x y]
-    #{x y}))
+(square-1 9)
+
+(defn square
+  "Doc String for square func, use it to document what the function does"
+  [x]
+  (println "Calculating the square of x: " x)
+  (* x x))
+
+(square 12)
+
+;; Multi-arity Functions
+;; Several ways to define functions with a variable number of arguments
+(def my-str "Hello World!")
+(println my-str)
 
 (defn make-set
   ([x] #{x})
   ([x y] #{x y})
   ([x y & zs] (clojure.set/union #{x y} (apply make-set zs))))
 
-(def my-set-1 (make-set-1 1 2))
-
 (def my-set (make-set 1 2 3 4))
-
-(def my-str "Hello World!")
-
-(println my-str)
 (println my-set)
+
+(def make-set-1
+  (fn [x y]
+    #{x y}))
+
+(def my-set-1 (make-set-1 1 2))
 (println my-set-1)
 
 
 ;; Using anonymous functions
 
 (def make-list-0 #(list))
-
 (make-list-0)
 
 (def make-list-1 #(list %1 %2))
-
 (make-list-1 6 7)
 
 (def make-list-2 #(list %1 %2 %&))
-
 (make-list-2 6 7 8 9)
 
 ;; LOOPS - RECUR
@@ -48,7 +60,9 @@
 
 (print-down-from 6)
 
-(defn sum-down-from [sum x]
+(defn sum-down-from
+  "Sum all numbers from x to 1 in decreasing order"
+   [sum x]
   (if (pos? x)
     (recur (+ sum x) (dec x))
     sum))
@@ -62,3 +76,5 @@
       sum)))
 
 (sum-down-from-1 10)
+
+;; Note: RECUR must be in a tail position, that is, last instructiohn of an execution branch\
